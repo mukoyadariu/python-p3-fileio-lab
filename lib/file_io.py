5ceda00b3e9137318398e268fb882767c445dfb8
mukoyadariu <mukoya.darius@student.moringaschool.com>
2023-08-30 09:50:34 +0300

    completed assignment

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,18 @@
 def write_file(file_name, file_content):
-    pass
+    with open(f'{file_name}.txt', 'w') as f:
+        f.write(file_content)
 
 def append_file(file_name, append_content):
-    pass
+    with open(f'{file_name}.txt', 'a') as f:
+        f.write(append_content)
 
 def read_file(file_name):
-    pass
+    with open(f'{file_name}.txt', 'r') as f:
+        return f.read()
+
+if __name__ == "__main__":
+    # Example usage
+    write_file("example", "This is an example.")
+    append_file("example", "\nThis is an appended line.")
+    content = read_file("example")
+    print(content)
